dbb0df2b6c1a713d65021e9c8fc6db0a6cd776ac
Karina Freitas <karinaglf@gmail.com>
2022-06-03 12:46:31 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const navItems = [
+	{ label: 'Home', icon: './assets/icon-home.svg' },
+	{ label: 'Explore', icon: './assets/icon-explore.svg' },
+	{ label: 'Notifications', icon: './assets/icon-notifications.svg' },
+	{ label: 'Messages', icon: './assets/icon-messages.svg' },
+	{ label: 'Bookmarks', icon: './assets/icon-bookmarks.svg' },
+	{ label: 'Lists', icon: './assets/icon-lists.svg' },
+	{ label: 'Profile', icon: './assets/icon-profile.svg' },
+	{ label: 'More', icon: './assets/icon-more.svg' },
+];
---
+++ tweets.js
@@ -0,0 +1,101 @@
+const tweetsData = [
+	{
+		id: '807095',
+		name: 'The New York Times',
+		username: 'nytimes',
+		verified: true,
+		avatar: 'https://pbs.twimg.com/profile_images/1098244578472280064/gjkVMelR_normal.png',
+		tweets: [
+			{
+				userid: '807095',
+				tweet:
+					'Queen Elizabeth II will not attend the #PlatinumJubilee thanksgiving service after experiencing discomfort on a busy first day of festivities, Buckingham Palace said. https://t.co/J8jyo0jPsF',
+			},
+			{
+				userid: '807095',
+				tweet:
+					'The #PlatinumJubilee service of thanksgiving at St. Paul\u2019s Cathedral in London is intended to exemplify Queen Elizabeth II\u2019s special role in British life as both head of state and of the Church of England.\n\nFollow along live. https://t.co/8iuklJ7Kbx',
+			},
+			{
+				userid: '807095',
+				tweet:
+					'Barack Obama paid tribute to Queen Elizabeth II in a video message in which he said they had formed their own \u201cspecial relationship.\u201d https://t.co/eAqQqgn0Qm https://t.co/0LIP3q2phX',
+			},
+			{
+				userid: '807095',
+				tweet:
+					'Decades after militant violence forced Hindus out of the disputed region, Kashmiri Hindus say they have been shaken by an intensifying spate of targeted killings \u2014 and that they desperately want out, yet again. https://t.co/TP9Zjd5REh',
+			},
+			{
+				userid: '807095',
+				tweet:
+					"Prince Harry and his wife, Meghan, are expected to make their first big appearance at Queen Elizabeth II's #PlatinumJubilee.\n\nThey will attend a service at St. Paul\u2019s Cathedral, but the queen herself will skip the event. Follow live. https://t.co/LeusTsxQqq",
+			},
+		],
+	},
+	{
+		id: '32140588',
+		name: 'Jonathan Abrams',
+		username: 'Jpdabrams',
+		verified: true,
+		avatar: 'https://pbs.twimg.com/profile_images/666819980164399105/aTmRtrOv_normal.jpg',
+		tweets: [
+			{ userid: '32140588', tweet: '@JimMWeber Thank you!' },
+			{
+				userid: '32140588',
+				tweet:
+					'David Simon and Ed Burns discuss the legacy of their seminal crime drama, and why the systemic decay it depicted has become only more profound.\n\nhttps://t.co/R62hOtdXqm',
+			},
+			{ userid: '32140588', tweet: 'Have you pre-ordered yet? If not, the link is below! https://t.co/eyHdFwBsan' },
+			{ userid: '32140588', tweet: 'Thank you! https://t.co/qIX4RxhygK' },
+		],
+	},
+	{
+		id: '72954989',
+		name: 'Christina Jewett',
+		username: 'By_CJewett',
+		verified: true,
+		avatar: 'https://pbs.twimg.com/profile_images/558689214347239427/J-HJdU7A_normal.jpeg',
+		tweets: [
+			{ userid: '72954989', tweet: 'https://t.co/KCWopfpfBZ via @shearm' },
+			{ userid: '72954989', tweet: 'https://t.co/3kZzfQz9Un' },
+			{ userid: '72954989', tweet: 'https://t.co/o8LPvUtlYo' },
+			{ userid: '72954989', tweet: 'More of our coverage here: https://t.co/3SnpugjbHD' },
+			{
+				userid: '72954989',
+				tweet:
+					'The Health and Human Services Inspector General announces an investigation into "whether FDA followed the inspections and recall process for infant formula in accordance with Federal requirements:" https://t.co/9gQZZr3ZNE',
+			},
+		],
+	},
+	{
+		id: '1072311156151410694',
+		name: 'Troy Closson',
+		username: 'troy_closson',
+		verified: false,
+		avatar: 'https://pbs.twimg.com/profile_images/1378715585979252736/F7OlnUho_normal.jpg',
+		tweets: [
+			{ userid: '1072311156151410694', tweet: '@errinhaines thanks for this message, and for reading Errin!' },
+			{
+				userid: '1072311156151410694',
+				tweet:
+					'Many Black Buffalo families felt they would only ever remain a priority for so long.\n\nBut how their lives changed because of a racist massacre is essential to grasp, and not forget. So here\u2019s a story about what it did to one \u2014 and a gift link to read it: https://t.co/uSCjzIAqr4',
+			},
+			{
+				userid: '1072311156151410694',
+				tweet:
+					'Celestine\u2019s relatives just buried her, and returned home from a repast. They turned on CNN. The chyron: "Texas Rangers say 18 children, 3 adults dead.\u201d\n\nSome cried. Others were anxious. But they all knew it meant the national attention would fade. \nhttps://t.co/uSCjzIAqr4',
+			},
+			{
+				userid: '1072311156151410694',
+				tweet:
+					'The U.S. has seen more than 210 mass shootings in 2022, at least four people killed or injured.\n\nIt is not easy to process. Yet each one leaves something behind. For Celestine, it was her last living sister, only son, nine grandchildren and dozens more. https://t.co/nCznZKF5fQ',
+			},
+			{
+				userid: '1072311156151410694',
+				tweet:
+					'Celestine Chaney\u2019s family allowed @GabrielaBhaskar and I to join them as they prepared for her funeral. \n\nWe went to their family game night. Suit shopping. A tattoo parlor. Each moment showed the grief that would remain, even after the next tragedy arose. https://t.co/uSCjzIAqr4',
+			},
+		],
+	},
+];
